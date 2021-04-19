ef15ea56fc011d03ab07240d7581268ba18af3e4
gygus7345 <gygus7345@naver.com>
2021-04-19 23:24:35 +0900

    ㅇ

--- TeamProject01/src/com/Team/Client/util/Gmail.java
+++ TeamProject01/src/com/Team/Client/util/Gmail.java
@@ -7,7 +7,7 @@ public class Gmail extends Authenticator{
 	
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("gygus7345","wkrmatjd12!");
+		return new PasswordAuthentication("본인의 구글 아이디 @필요없습니다.","본인의 구글 비밀번호");
 	}
 	
 }	
